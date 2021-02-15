4ec014c7b99c4aeb03479a60605bb0f661375490
irinavasilenko88 <vasilenko_i@yahoo.com>
2021-02-15 13:10:43 +0300

    code changed

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,27 +1,39 @@
 package ru.netology.page;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
+import ru.netology.data.UserInfo;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    public VerificationPage validLogin() {
-        $("[data-test-id=login] input").setValue(DataGenerator.generateUserInfo().getLogin());
-        $("[data-test-id=password] input").setValue(DataGenerator.generateUserInfo().getPassword());
-        $("[data-test-id=action-login]").click();
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton =  $("[data-test-id=action-login]");
+
+    public VerificationPage validLogin(UserInfo userInfo) {
+        loginField.setValue(DataGenerator.generateUserInfo().getLogin());
+        passwordField.setValue(DataGenerator.generateUserInfo().getPassword());
+        loginButton.click();
         return new VerificationPage();
     }
 
-    public LoginPage generateInvalidLogin() {
-        $("[data-test-id=login] input").setValue(DataGenerator.getInvalidUserInfo().getLogin());
-        $("[data-test-id=password] input").setValue(DataGenerator.generateUserInfo().getPassword());
-        $("[data-test-id=action-login]").click();
+    public LoginPage generateInvalidLogin(UserInfo userInfo) {
+        loginField.setValue(DataGenerator.getInvalidUserInfo().getLogin());
+        passwordField.setValue(DataGenerator.getInvalidUserInfo().getPassword());
+        loginButton.click();
         return new LoginPage();
 
     }
 
     public void blockNotification() {
         $("[data-test-id='error-notification']").shouldHave(Condition.text("Система заблокирована"));
+    }
+
+    public void cleanFields(UserInfo userInfo){
+        loginField.doubleClick().sendKeys(Keys.BACK_SPACE);
+        passwordField.doubleClick().sendKeys(Keys.BACK_SPACE);
     }
 }
